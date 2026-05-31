7bdd89f6b8805a4329b415ace0c0fb368742ce05
pengtao.deng <pengtao.deng@flightroutes24.com>
2026-05-31 13:03:43 +0800

    fix(web): prefix header brand with FR24

--- web/src/shared/lib/brand.ts
+++ web/src/shared/lib/brand.ts
@@ -1,6 +1,6 @@
 export const appBrand = {
   companyShortName: 'FR24',
-  productName: 'SkillHub',
+  productName: 'FR24 SkillHub',
   displayName: 'FR24 SkillHub',
   logoUrl: '/fr24-logo-white.svg',
   homepageUrl: 'https://www.flightroutes24.com/',
