e7dd8dd5cc2177ec16e8f57966f668f8d59c15b6
pengtao.deng <pengtao.deng@flightroutes24.com>
2026-06-04 20:30:30 +0800

    fix(fr24 login): 修复

--- web/public/runtime-config.js
+++ web/public/runtime-config.js
@@ -7,5 +7,5 @@ window.__SKILLHUB_RUNTIME_CONFIG__ = {
   authSessionBootstrapProvider: '',
   authSessionBootstrapAuto: 'false',
   authPrivateSsoTwoFactorEnabled: 'true',
-  authPrivateSsoSm2PublicKey: '04eb5a97ac9efcfd5530d57643d6f4612c191e3071f45b3117a90175d4422eb6160da11ef707ef7c32348e1c04e3f4d33cc2ff833acea2a412418a4330c7f11e40',
+  authPrivateSsoSm2PublicKey: 'eb5a97ac9efcfd5530d57643d6f4612c191e3071f45b3117a90175d4422eb6160da11ef707ef7c32348e1c04e3f4d33cc2ff833acea2a412418a4330c7f11e40',
 }
